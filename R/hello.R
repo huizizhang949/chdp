8c5dffee11670c2d5100f5b9e55b1093d1dc40f6
Huizi Zhang <zhanghuizi98@outlook.com>
2024-01-30 22:41:10 +0800

    Second commit

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,15 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
+#' Personal greeting
+#'
+#' @description Greet a person and appropriately capitalize their name.
+#'
+#' @param name Your name (character string; e.g. "john doe").
+#'
+#' @return A character string, capitalized to title case.
+#' @export
+#'
+#' @examples
+#' hello("james bond")
+hello <- function(name = "your name") {
+  name <- stringr::str_to_title(name)
+  print(paste("Hello,", name))
 }
